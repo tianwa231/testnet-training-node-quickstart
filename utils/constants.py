2d6b84a28049b885a5846d063d35827a8af9e4ba
tianwa231 <tianw231@gmail.com>
2025-01-11 14:11:17 +0800

    Update constants.py

--- utils/constants.py
+++ utils/constants.py
@@ -143,10 +143,10 @@ model2template = {
     "meta-llama/Meta-Llama-3-70B": llama3_template,
     "meta-llama/Meta-Llama-3-70B-Instruct": llama3_template,
      # phi3
-    "microsoft/Phi-3.5-mini-instruct",
-    "microsoft/Phi-3-mini-4k-instruct",
-    "microsoft/Phi-3-small-8k-instruct",
-    "microsoft/Phi-3-medium-4k-instruct",
+    "microsoft/Phi-3.5-mini-instruct":phi3_template,
+    "microsoft/Phi-3-mini-4k-instruct":phi3_template,
+    "microsoft/Phi-3-small-8k-instruct":phi3_template,
+    "microsoft/Phi-3-medium-4k-instruct":phi3_template,
 }
 
 model2size = {
